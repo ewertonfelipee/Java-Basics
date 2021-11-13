ad9e50d0873432a38e6e66c5769caa439946e7b0
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2021-11-13 17:29:30 -0300

    Create EncapsulationMain.java

---
+++ EncapsulationMain.java
@@ -0,0 +1,7 @@
+public class EncapsulationMain {
+    public static void main(String[] args){
+        Encapsulation Person = new Encapsulation();
+        Person.setName("Ewerton");// Set the Value of the name variable to "Ewerton"
+        System.out.println(Person.getName());
+    }
+}
