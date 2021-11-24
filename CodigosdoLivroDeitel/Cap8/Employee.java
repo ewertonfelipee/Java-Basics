9bd1e17193173940bc9754295125f8995f7b580b
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2021-11-23 22:16:43 -0300

    Code Increment in Employeer e EmployeerTest Classes

--- CodigosdoLivroDeitel/Cap8/Employee.java
+++ CodigosdoLivroDeitel/Cap8/Employee.java
@@ -3,7 +3,9 @@ package CodigosdoLivroDeitel.Cap8;
 
 public class Employee
 {
-    private String firstName;
+    private
+        static int count = 0;
+        String firstName;
         String lastName;
         Date birthDate;
         Date hireDate; // variaveis de instancia referencias a classe Date
@@ -16,12 +18,47 @@ public class Employee
         this.lastName = lastName;
         this.birthDate = birthDate;
         this.hireDate = hireDate;
+
+        ++count;
+        System.out.printf("Employee constructor: %s %s; Birthday: %s Hire: %s; count = %d%n", 
+            firstName, lastName, birthDate, hireDate, count);
+    }
+
+    // obtem o primeiro nome
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    //obtem o ultimo nome
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    // obtem a data de nascimento
+    public Date getBirthDate()
+    {
+        return birthDate;
+    }
+
+    // obtem a data de contratacao
+    public Date getHireDate()
+    {
+        return hireDate;
+    }
+
+    // metodo estatico para obter valor de contagem de static
+    // retorna o numero de objetos Employee que foram criados ate agora
+    public static int getCount()
+    {
+        return count;
     }
 
     // converte Employee em formato de string
-    public String toString()
-    {
-        return String.format("%s, %s    Hired: %s  Birthday:   %s", 
-            lastName, firstName, hireDate, birthDate);
-    }
+    // public String toString()
+    // {
+    //     return String.format("%s, %s    Hired: %s  Birthday:   %s", 
+    //         lastName, firstName, hireDate, birthDate);
+    // }
 } // fim da classe
--- CodigosdoLivroDeitel/Cap8/EmployeeTest.java
+++ CodigosdoLivroDeitel/Cap8/EmployeeTest.java
@@ -4,11 +4,29 @@ package CodigosdoLivroDeitel.Cap8;
 public class EmployeeTest
 {
     public static void main(String[] args)
-    {
-        Date birth = new Date(5, 23, 1998);
-        Date hire = new Date(7, 11, 2022);
-        Employee employee = new Employee("Ewerton", "Felipe", birth, hire);
+    {   
+        //mostra que a contafem eh 0 antes de criar Employees
+        System.out.printf("%nEmployee before instantiation: %d%n",
+            Employee.getCount());
 
-        System.out.println(employee);
+        // cria objetos da classe Date
+        Date birthe1 = new Date(5, 23, 1998);
+        Date hiree1 = new Date(7, 11, 2022);
+        Date birthe2 = new Date(10, 12, 1972);
+        Date hiree2 = new Date(9, 15, 2004);
+
+        // cria objetos da classe Employee
+        Employee e1 = new Employee("Ewerton", "Felipe", birthe1, hiree1);
+        Employee e2 = new Employee("Maria", "Betania", birthe2, hiree2);
+
+        // mostra que a contagem eh 2 depois de criar dois Employess
+        System.out.printf("%nEmployees after instantiation:%n");
+        System.out.printf("via e1.getCount(): %d%n", e1.getCount());
+        System.out.printf("via e2.getCount(): %d%n", e2.getCount());
+        System.out. printf("via Employee.getCount(): %d%n", Employee.getCount());
+
+        System.out.printf("%nEmployee 1: %s %s%nEmployee 2: %s %s%n",
+            e1.getFirstName(), e1.getLastName(), 
+            e2.getFirstName(), e2.getLastName());
     }
 } // fim da classe
