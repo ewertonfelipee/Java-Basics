db4a3a19500aa2b490b639e15abec1264979d1fa
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2022-04-04 16:05:15 -0300

    Modifies

---
+++ cursoloiane/javabasico/Variaveis.java
@@ -0,0 +1,13 @@
+package cursoloiane.javabasico;
+
+public class Variaveis {
+    public static void main(String[] args){
+        int idade = 23;
+        char c = 'a';
+        String meuNome = "ewerton";
+
+        System.out.println(idade);
+        System.out.println(c);
+        System.out.println(meuNome);
+    }
+}
